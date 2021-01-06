718092df060fa52b9e2c390b299f0e9d9d6ca3f4
sebastientromp <sebastien.tromp@gmail.com>
2021-01-06 13:23:42 +0100

    add getBaseCardId method

--- projects/reference-data/src/lib/services/all-cards.service.ts
+++ projects/reference-data/src/lib/services/all-cards.service.ts
@@ -1,3 +1,4 @@
+import { CardIds } from '../card-ids';
 import { ReferenceCard } from '../models/reference-cards/reference-card';
 import { http } from './utils';
 
@@ -78,3 +79,164 @@ export class AllCardsService {
 		});
 	}
 }
+
+// The spellstones are present in the AI decklist in their basic version
+// However, if the AI plays the spellstone's upgraded version, we need to remove
+// the basic one from the decklist
+export const getBaseCardId = (cardId: string): string => {
+	if (!cardId) {
+		return null;
+	}
+
+	switch (cardId) {
+		// The spellstones
+		case 'LOOT_103t1':
+		case 'LOOT_103t2':
+			return 'LOOT_103';
+		case 'LOOT_043t2':
+		case 'LOOT_043t3':
+			return 'LOOT_043';
+		case 'LOOT_051t1':
+		case 'LOOT_051t2':
+			return 'LOOT_051';
+		case 'LOOT_064t1':
+		case 'LOOT_064t2':
+			return 'LOOT_064';
+		case 'LOOT_080t2':
+		case 'LOOT_080t3':
+			return 'LOOT_080';
+		case 'LOOT_091t1':
+		case 'LOOT_091t2':
+			return 'LOOT_091';
+		case 'LOOT_203t2':
+		case 'LOOT_203t3':
+			return 'LOOT_203';
+		case 'LOOT_503t':
+		case 'LOOT_503t2':
+			return 'LOOT_503';
+		case 'LOOT_507t':
+		case 'LOOT_507t2':
+			return 'LOOT_507';
+		case 'FB_Champs_LOOT_080t2':
+		case 'FB_Champs_LOOT_080t3':
+			return 'FB_Champs_LOOT_080';
+		// The "unidentified" spells
+		case 'LOOT_278t1':
+			return 'LOOT_278';
+		case 'LOOT_278t2':
+			return 'LOOT_278';
+		case 'LOOT_278t3':
+			return 'LOOT_278';
+		case 'LOOT_278t4':
+			return 'LOOT_278';
+		case 'LOOT_285t':
+			return 'LOOT_285';
+		case 'LOOT_285t2':
+			return 'LOOT_285';
+		case 'LOOT_285t3':
+			return 'LOOT_285';
+		case 'LOOT_285t4':
+			return 'LOOT_285';
+		case 'LOOT_286t1':
+			return 'LOOT_286';
+		case 'LOOT_286t2':
+			return 'LOOT_286';
+		case 'LOOT_286t3':
+			return 'LOOT_286';
+		case 'LOOT_286t4':
+			return 'LOOT_286';
+		case 'DAL_366t1':
+			return 'DAL_366';
+		case 'DAL_366t2':
+			return 'DAL_366';
+		case 'DAL_366t3':
+			return 'DAL_366';
+		case 'DAL_366t4':
+			return 'DAL_366';
+		// Galakrond
+		case 'DRG_600t2':
+			return 'DRG_600';
+		case 'DRG_600t3':
+			return 'DRG_600';
+		case 'DRG_610t2':
+			return 'DRG_610';
+		case 'DRG_610t3':
+			return 'DRG_610';
+		case 'DRG_620t2':
+			return 'DRG_620';
+		case 'DRG_620t3':
+			return 'DRG_620';
+		case 'DRG_650t2':
+			return 'DRG_650';
+		case 'DRG_650t3':
+			return 'DRG_650';
+		case 'DRG_660t2':
+			return 'DRG_660';
+		case 'DRG_660t3':
+			return 'DRG_660';
+		// Corrupted
+		case 'DMF_054t':
+			return 'DMF_054';
+		case 'DMF_061t':
+			return 'DMF_061';
+		case 'DMF_064t':
+			return 'DMF_064';
+		case 'DMF_073t':
+			return 'DMF_073';
+		case 'DMF_078t':
+			return 'DMF_078';
+		case 'DMF_080t':
+			return 'DMF_080';
+		case 'DMF_082t':
+			return 'DMF_082';
+		case 'DMF_083t':
+			return 'DMF_083';
+		case 'DMF_090t':
+			return 'DMF_090';
+		case 'DMF_101t':
+			return 'DMF_101';
+		case 'DMF_105t':
+			return 'DMF_105';
+		case 'DMF_117t':
+			return 'DMF_117';
+		case 'DMF_117t2':
+			return 'DMF_117';
+		case 'DMF_118t':
+			return 'DMF_118';
+		case 'DMF_124t':
+			return 'DMF_124';
+		case 'DMF_163t':
+			return 'DMF_163';
+		case 'DMF_174t':
+			return 'DMF_174';
+		case 'DMF_184t':
+			return 'DMF_184';
+		case 'DMF_186a':
+			return 'DMF_186';
+		case 'DMF_244t':
+			return 'DMF_244';
+		case 'DMF_247t':
+			return 'DMF_247';
+		case 'DMF_248t':
+			return 'DMF_248';
+		case 'DMF_517a':
+			return 'DMF_517';
+		case 'DMF_526a':
+			return 'DMF_526';
+		case 'DMF_701t':
+			return 'DMF_701';
+		case 'DMF_703t':
+			return 'DMF_703';
+		case 'DMF_730t':
+			return 'DMF_730';
+		case 'DMF_526a':
+			return 'DMF_526';
+		// Transfer Student should be handled on a case by case basis
+	}
+
+	if (cardId.startsWith(CardIds.Collectible.Neutral.TransferStudent)) {
+		return CardIds.Collectible.Neutral.TransferStudent;
+	}
+
+	return cardId;
+};
