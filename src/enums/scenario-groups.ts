6eeaa1f3ef8bc5757046be950f17f398c78783b7
sebastientromp <sebastien.tromp@gmail.com>
2021-11-30 20:17:44 +0100

    update enums for 126807
    update enums for 126807
    
    update enums for 126807

--- src/enums/scenario-groups.ts
+++ src/enums/scenario-groups.ts
@@ -15,7 +15,7 @@ export const ARENAS = [
 	ScenarioId._200_ARENA_SEASON,
 	ScenarioId._204_ARENA_SEASON,
 	ScenarioId._210_ARENA_SEASON_,
-	ScenarioId._220_ARENA_SEASON_,
+	ScenarioId._220_ARENA_SEASON,
 ];
 
 export const DALARAN_HEIST_ALL = [
