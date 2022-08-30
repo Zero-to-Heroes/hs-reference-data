49a09de15ec96e4d736d657d31745f0a4bd5a87d
sebastientromp <sebastien.tromp@gmail.com>
2022-08-30 19:43:21 +0200

    update enums for 148211

---
+++ ref/enums/148211.block-type.ts
@@ -0,0 +1,15 @@
+export enum BlockType {
+	INVALID = 0,
+	ATTACK = 1,
+	JOUST = 2,
+	POWER = 3,
+	TRIGGER = 5,
+	DEATHS = 6,
+	PLAY = 7,
+	FATIGUE = 8,
+	RITUAL = 9,
+	REVEAL_CARD = 10,
+	GAME_RESET = 11,
+	MOVE_MINION = 12,
+	TRADE = 13,
+}
