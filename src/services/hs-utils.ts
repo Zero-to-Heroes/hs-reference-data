c5b2e8443d5a19469edca372aa91867d61bab13d
sebastientromp <sebastien.tromp@gmail.com>
2023-12-14 09:29:57 +0100

    update enums for 190920

--- src/services/hs-utils.ts
+++ src/services/hs-utils.ts
@@ -1,6 +1,6 @@
 import { GameFormatString } from '../enums/game-format';
 import { ReferenceCard } from '../models/reference-cards/reference-card';
-import { GameFormat } from '../public-api';
+import { GameFormat, GameType } from '../public-api';
 import { AllCardsService } from './all-cards.service';
 
 export const ALL_CLASSES = [
@@ -28,6 +28,55 @@ export const formatFormat = (format: GameFormat): GameFormatString => {
 		case GameFormat.FT_WILD:
 		default:
 			return 'wild';
+	}
+};
+
+export const formatGameType = (gameType: GameType): string => {
+	switch (gameType) {
+		case GameType.GT_UNKNOWN:
+			return 'unknown';
+		case GameType.GT_VS_AI:
+			return 'practice';
+		case GameType.GT_VS_FRIEND:
+			return 'friendly';
+		case GameType.GT_TUTORIAL:
+			return 'tutorial';
+		case GameType.GT_ARENA:
+			return 'arena';
+		case GameType.GT_RANKED:
+			return 'ranked';
+		case GameType.GT_CASUAL:
+			return 'casual';
+		case GameType.GT_TAVERNBRAWL:
+		case GameType.GT_TB_1P_VS_AI:
+		case GameType.GT_TB_2P_COOP:
+		case GameType.GT_FSG_BRAWL_VS_FRIEND:
+		case GameType.GT_FSG_BRAWL:
+		case GameType.GT_FSG_BRAWL_1P_VS_AI:
+		case GameType.GT_FSG_BRAWL_2P_COOP:
+			return 'tavern-brawl';
+		case GameType.GT_BATTLEGROUNDS:
+		case GameType.GT_BATTLEGROUNDS_FRIENDLY:
+		case GameType.GT_BATTLEGROUNDS_AI_VS_AI:
+		case GameType.GT_BATTLEGROUNDS_PLAYER_VS_AI:
+			return 'battlegrounds';
+		case GameType.GT_MERCENARIES_AI_VS_AI:
+			return 'mercenaries-ai-vs-ai';
+		case GameType.GT_MERCENARIES_FRIENDLY:
+			return 'mercenaries-friendly';
+		case GameType.GT_MERCENARIES_PVE:
+			return 'mercenaries-pve';
+		case GameType.GT_MERCENARIES_PVP:
+			return 'mercenaries-pvp';
+		case GameType.GT_MERCENARIES_PVE_COOP:
+			return 'mercenaries-pve-coop';
+		case GameType.GT_PVPDR:
+			return 'duels';
+		case GameType.GT_PVPDR_PAID:
+			return 'paid-duels';
+		default:
+			console.warn('unsupported game type', gameType);
+			return 'unknown';
 	}
 };
 
