fc59a5122f995c1d9910434636913289eb609e3f
sebastientromp <sebastien.tromp@gmail.com>
2026-08-15 23:52:48 +0200

    update reference cards for 248022
    update reference cards for 248022

--- src/models/reference-cards/bg-related-cards.ts
+++ src/models/reference-cards/bg-related-cards.ts
@@ -111,4 +111,6 @@ export const BG_RELATED_CARDS = {
 	[CardIds.TimewarpedLasso_BG34_Treasure_609]: [CardIds.EnchantedLasso_BG28_512],
 	[CardIds.LeylineSurfacer_BG35_881]: [CardIds.ArcaneAbsorption_BG35_911],
 	[CardIds.LeylineSurfacer_BG35_881_G]: [CardIds.ArcaneAbsorption_BG35_911],
+	[CardIds.FlitteringBat_BG36_200]: [CardIds.FlitteringBat_ForagingBatToken_BG36_200t],
+	[CardIds.FlitteringBat_BG36_200_G]: [CardIds.FlitteringBat_ForagingBatToken_BG36_200_Gt],
 };
