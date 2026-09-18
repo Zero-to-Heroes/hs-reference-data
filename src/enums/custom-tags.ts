3291b736a183b8c07829c9af619e93f7d1d89670
sebastientromp <sebastien.tromp@gmail.com>
2026-09-18 16:33:13 +0200

    add NEVER_DEALS_HERO_DAMAGE mechanics

--- src/enums/custom-tags.ts
+++ src/enums/custom-tags.ts
@@ -4,4 +4,6 @@ export enum CustomTags {
 	TARGETS_FRIENDLY_MINION,
 	TARGETS_FRIENDLY_CHARACTER,
 	TARGETS_ENEMY,
+	/** Charge-style minions that can target heroes but never deal hero damage (e.g. The Living Plague). */
+	NEVER_DEALS_HERO_DAMAGE,
 }
